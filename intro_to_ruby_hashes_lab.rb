ad7f2c6a7c9b82201235b5dc83e4f677d59119a6
Jacob Squier <jacob.squier@gmail.com>
2020-06-25 10:41:51 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+  hash = { :railroads => {} }
+  return hash
 
 end
 
@@ -9,7 +11,13 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  newHash = base_hash()
+  newHash[:railroads] = {
+    pieces: 4,
+    :rent_in_dollars => {},
+    :names => {}
+  }
+  return newHash
 end
 
 def monopoly_with_third_tier
@@ -17,6 +25,21 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly = monopoly_with_second_tier()
+  monopoly[:railroads][:rent_in_dollars] = {
+      one_piece_owned: 25,
+      two_pieces_owned: 50,
+      three_pieces_owned: 100,
+      four_pieces_owned: 200
+  }
+  monopoly[:railroads][:names] = {
+    :reading_railroad => {},
+    :pennsylvania_railroad => {},
+    :b_and_o_railroad => {},
+    :shortline_railroad => {}
+  }
+  return monopoly
+  
 
 end
 
@@ -25,5 +48,18 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  monopoly = monopoly_with_third_tier()
+  monopoly[:railroads][:names][:reading_railroad] = {
+    mortgage_value: 100
+  }
+  monopoly[:railroads][:names][:pennsylvania_railroad] = {
+    mortgage_value: 200
+  }
+  monopoly[:railroads][:names][:b_and_o_railroad] = {
+    mortgage_value: 400
+  }
+  monopoly[:railroads][:names][:shortline_railroad] = {
+    mortgage_value: 800
+  }
+  return monopoly
 end
